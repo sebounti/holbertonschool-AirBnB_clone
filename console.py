f363390189a894e4b8beddde95bee0565553ad88
nimrod02 <letrun.a@gmail.com>
2023-07-05 15:34:52 +0200

    fix console for emptyline and pycodestyle

--- console.py
+++ console.py
@@ -3,23 +3,24 @@
 command module
 """
 
-import cmd, sys
+import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
-        prompt = "(hbnb) "
-        
-        def do_quit(self, arg):
-                "Quit command to exit the program"
-                return True
-        
-        def do_EOF(self, arg):
-                "End-of-file : a code, maker, or signal used to indicate the end of a file of data"
-                return True
-                
-        def do_help(self, arg):
-                "help command if you need help"
-                return super().do_help(arg)
-        
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """End-of-file : used to indicate the end of a file of data"""
+        return True
+
+    def emptyline(self):
+        pass
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
