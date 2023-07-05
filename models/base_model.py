6da5fd21d25c7dfb05d49f0e01c80bc0436ef0db
lenne sebastien <sebastienlenne@gmail.com>
2023-07-05 12:54:04 +0200

    update file base model.py task 3 project console

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 '''Defines the BaseModel class.'''
 
 from datetime import datetime
-from uuid import uuid4
+import uuid
 
 
 class BaseModel:
@@ -14,11 +14,32 @@ class BaseModel:
            Initialization of the base model
 
         """
-        self.id = str(uuid4)
-        self.created_at = self.updated_at = datetime.utcnow
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key =="updated_at":
-                    value == datetime.strftime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strftime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+
+    def __str__(self):
+        """
+        Returns
+        """
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the updated_at attribute
+        with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''
+        '''
+        dictionary = self.__dict__
+        dictionary["__class__"] = str(type(self).__name__)
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
