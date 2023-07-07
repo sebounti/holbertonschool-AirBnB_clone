baf218413db6de8cb1a0b969e2b4924a5a03a2ff
nimrod02 <letrun.a@gmail.com>
2023-07-07 09:00:13 +0200

    fix the console problemes with destroy and update, and add method line 23 in BaseModel

--- console.py
+++ console.py
@@ -163,3 +163,4 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+    
--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            self.first_name = ""
             models.storage.new(self)
             models.storage.save()
         else:
